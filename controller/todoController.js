f4bf8869d7f0afb98f521cbd04ae0303793aca72
Kalana Hettiarachchi <kalanahettiarachchi1998@gmail.com>
2019-12-31 23:06:22 +0530

    Tutorial 4 done

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,11 +1,23 @@
-module.exports = function(app){
-    app.get('/todo',function(req, res){
-        res.render('todo');
-    });
-    app.post('/todo',function(req, res){
 
+var bodyParser = require('body-parser');
+
+var data = [{item:'Go to work'},{item:'Have lunch'},{item:'Sleep'}];
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+module.exports = function(app){
+
+    app.get('/todo',function(req, res){
+        res.render('todo',{todos:data});
     });
+
+    app.post('/todo', urlencodedParser, function(req, res){
+        data.push(req.body);
+        res.json(data);
+    });
+
     app.delete('/todo',function(req, res){
 
     });
+    
 };
