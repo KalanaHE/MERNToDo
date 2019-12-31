bf2e10de035b37ed6874e8c9961c89619aa58a66
Kalana Hettiarachchi <kalanahettiarachchi1998@gmail.com>
2020-01-01 00:34:35 +0530

    Tutorial 6 done

--- controller/todoController.js
+++ controller/todoController.js
@@ -11,10 +11,7 @@ var todoSchema = new mongoose.Schema({
 });
 
 var Todo = mongoose.model('Todo', todoSchema);
-var itemOne = Todo({item: 'Buy flowers for her'}).save(function(err){
-    if(err) throw err;
-    console.log('Item saved');
-});
+
 
 //var data = [{item:'Go to work'},{item:'Have lunch'},{item:'Sleep'}];
 
@@ -23,16 +20,27 @@ var urlencodedParser = bodyParser.urlencoded({extended: false});
 module.exports = function(app){
 
     app.get('/todo',function(req, res){
-        res.render('todo',{todos:data});
+        //get data from mongoDB and pass it to view
+        Todo.find({}, function(err, data){
+            if(err) throw err;
+            res.render('todo',{todos:data});
+        });
     });
 
     app.post('/todo', urlencodedParser, function(req, res){
-        data.push(req.body);
-        res.json(data);
+        //get data from the view and add it to mongodb
+        var newTodo = Todo(req.body).save(function(err, data){
+            if(err) throw err;
+            res.json(data);
+        });
     });
 
-    app.delete('/todo',function(req, res){
-
+    app.delete('/todo/:item',function(req, res){
+        //delete the requested item from db
+        Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+            if(err) throw err;
+            res.json(data);
+        });
     });
     
 };
