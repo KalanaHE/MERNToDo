70894da97b2c39700733b8b14b4c4754a39ebdca
Kalana Hettiarachchi <kalanahettiarachchi1998@gmail.com>
2019-12-31 23:58:14 +0530

    Tutorial 5 done

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,5 +1,20 @@
 
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//connect to DB
+mongoose.connect('mongodb+srv://calculus:helloworld@todo-uwl1f.mongodb.net/test?retryWrites=true&w=majority')
+
+//create a schema (the blueprint)
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'Buy flowers for her'}).save(function(err){
+    if(err) throw err;
+    console.log('Item saved');
+})
 
 var data = [{item:'Go to work'},{item:'Have lunch'},{item:'Sleep'}];
 
