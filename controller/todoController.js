d0f960435836764de543cfa5f8b40629c977fb0c
Kalana Hettiarachchi <kalanahettiarachchi1998@gmail.com>
2020-01-01 00:17:14 +0530

    Tutorial 5 done

--- controller/todoController.js
+++ controller/todoController.js
@@ -14,9 +14,9 @@ var Todo = mongoose.model('Todo', todoSchema);
 var itemOne = Todo({item: 'Buy flowers for her'}).save(function(err){
     if(err) throw err;
     console.log('Item saved');
-})
+});
 
-var data = [{item:'Go to work'},{item:'Have lunch'},{item:'Sleep'}];
+//var data = [{item:'Go to work'},{item:'Have lunch'},{item:'Sleep'}];
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
